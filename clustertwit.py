b82435bf9e25c89824766a0e3b53b1f3a01739b9
eldraco <eldraco@gmail.com>
2016-05-14 19:08:59 +0200

    Add support for twitter API with keys.
    - Also improve a little the grouping by adding blacklist of words.

--- clustertwit.py
+++ clustertwit.py
@@ -45,10 +45,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import sys
 import argparse
 
-
 twitts_groups=[]
 groups_id = 0
-version = '0.1'
+version = '0.3'
 
 class Twit():
     """ Class to hold all the info of one twit"""
@@ -58,6 +57,7 @@ class Twit():
         self.text = self.filtered_text
         self.v_text = self.text.split()
         self.group = False
+        self.duplicate = False
 
     def filter(self, text):
         """ Filter the original text to have:
@@ -67,12 +67,22 @@ class Twit():
         """
         result = []
         for word in text.split():
-            if len(word) > 5:
+            if len(word) > 5 and not self.is_blacklisted(word):
                 result.append(word.lower().strip().replace('\'','').replace('!','').replace('.',''))
         return ' '.join(iter(result))
     
     def __repr__(self):
         return 'Text: {}'.format(self.original_text)
+
+    def is_blacklisted(self, word):
+        blacklist = ['https']
+        if any(w in word for w in blacklist):
+            return True
+        return False
+    
+    def is_duplicate(self):
+        self.duplicate = True
+
 
 class TwitGroup():
     """ Class to hold a group of twits that are similar """
@@ -96,6 +106,8 @@ class TwitGroup():
             # Check if the current word is in this group
             try:
                 if self.group_words.index(word) >= 0:
+                    if args.verbose > 2:
+                        print '\t\tMatched: {}'.format(word)
                     amount += 1
             except ValueError:
                 # Not in the list
@@ -104,6 +116,7 @@ class TwitGroup():
             print '\tFinal amount: {}'.format(amount)
         # If all of the words in the twit matched... dont accept it. So we delete duplicate twits.
         if amount > 0 and amount == len(twit.v_text):
+            twit.is_duplicate()
             return False
         # Did we overcome the threshold?
         elif amount >= self.minimum_amount_of_matching_words:
@@ -140,29 +153,54 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-w', '--words', help='Minimum amount of words in each twit that should be equal to the words in the group.', action='store', default = 2, type=int, required=False)
 parser.add_argument('-t', '--twitts', help='Only show the groups with this amount of twitts inside.', action='store', default = 3, required=False, type=int)
 parser.add_argument('-v', '--verbose', help='Verbosity.', action='store', default = 1, required=False)
+parser.add_argument('-T', '--twitter', help='Use twitter API. See code for keys.', action='store_true', required=False)
 args = parser.parse_args()
 
+if args.twitter:
+    print 'Getting your tweets to the file twitter-cache.tmp'
+    try:
+        import tweepy
+    except ImportError:
+        print 'You need to install the library tweepy. Please do from the git repo https://github.com/tweepy/tweepy, if not it won\'t run'
+    from tweepy import OAuthHandler
+    # Put Here your keys
+    consumer_key = ''
+    consumer_secret = ''
+    access_token = ''
+    access_secret = ''
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    api = tweepy.API(auth)
+    file = open('./twitter-text-cache.tmp','a+')
+    file_all = open('./twitter-cache.tmp','a+')
+    for tweet in tweepy.Cursor(api.home_timeline).items(100):
+        # Process a single status
+        file.write(str(tweet.text.encode('utf-8') + '\n'))
+        file_all.write(str(tweet._json))
+        print tweet.text
+    file.close()
+    file_all.close()
+else:
+    # Start by reading the lines from stdin
+    for line in sys.stdin:
+        # Avoid empty lines
+        newtwit = Twit(line.strip())
+        if args.verbose > 1:
+            print 'Processing twit: {}'.format(newtwit.original_text)
+        for group in twitts_groups:
+            group.accept(newtwit)
+        # If the twit was not accepted, create a new group for it
+        if not newtwit.group and not newtwit.duplicate:
+            newgroup = TwitGroup(groups_id, args.words)
+            newgroup.add_twit(newtwit)
+            twitts_groups.append(newgroup)
+            groups_id += 1
 
-# Start by reading the lines from stdin
-for line in sys.stdin:
-    # Avoid empty lines
-    newtwit = Twit(line.strip())
-    if args.verbose > 1:
-        print 'Processing twit: {}'.format(newtwit.original_text)
+    # Print groups
+    print 
+    print 'Final Groups with more than {} twits.'.format(args.twitts)
     for group in twitts_groups:
-        group.accept(newtwit)
-    # If the twit was not accepted, create a new group for it
-    if not newtwit.group:
-        newgroup = TwitGroup(groups_id, args.words)
-        newgroup.add_twit(newtwit)
-        twitts_groups.append(newgroup)
-        groups_id += 1
-
-# Print groups
-print 
-print 'Final Groups with more than {} twits.'.format(args.twitts)
-for group in twitts_groups:
-    if len(group.twits) > args.twitts:
-        print group
-        group.print_twits()
+        if len(group.twits) > args.twitts:
+            print group
+            group.print_twits()
 
